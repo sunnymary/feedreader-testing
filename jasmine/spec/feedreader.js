5e57f3e73ef2ce3298c9d66434597efa74179faa
sunnymary <mary.zhou.coding@gmail.com>
2017-08-24 22:37:19 -0500

    add async testing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -76,20 +76,41 @@ $(function() {
 
     /* This is a new test suite named "Initial Entries" */
     describe("Initial Entries", function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This is a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, done);
+        });
+
+        it("should have at least one article when load feed", function(done) {
+            expect($(".feed").find(".entry")[0]).toBeDefined();
+            done();
+        })
 
     });
 
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
+    /* This is a new test suite named "New Feed Selection" */
+    describe("New Feed Selection", function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        var feedOne;
+        var feedTwo;
+        beforeEach(function(done) {
+            loadFeed(0, done);
+            feedOne = $(".feed")
+            loadFeed(1, done);
+            feedTwo = $(".feed");
+        });
+
+        it("should load new feed when load feed", function(){
+            expect(feedOne === feedTwo).toBe(false);
+        });
+    });
 }());
